c728cfc23f7b59df7ae69ffd0d17f81b1c0fde98
rema424 <rema.gapp@gmail.com>
2019-12-27 18:07:54 +0900

    wip: add test

--- sqlxx_test.go
+++ sqlxx_test.go
@@ -1,1 +1,23 @@
 package sqlxx
+
+import "testing"
+
+func TestMain(m *testing.M) {}
+
+func TestConnect(t *testing.T) {}
+
+func TestBuild(t *testing.T) {}
+
+func TestNewTxCtx(t *testing.T) {}
+
+func TestGet(t *testing.T) {}
+
+func TestSelect(t *testing.T) {}
+
+func TestExec(t *testing.T) {}
+
+func TestNamedExec(t *testing.T) {}
+
+func TestQuery(t *testing.T) {}
+
+func TestRunInTx(t *testing.T) {}
