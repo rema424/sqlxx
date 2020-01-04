dc4fb38ed9e4d708e5cbd83d319ba51df2d28685
rema424 <rema.gapp@gmail.com>
2020-01-04 12:27:06 +0900

    refacotr sqlxx_test.go

--- sqlxx_test.go
+++ sqlxx_test.go
@@ -167,207 +167,287 @@ func TestContext(t *testing.T) {
 }
 
 func TestMySQL(t *testing.T) {
-	testDB(t, db)
+	ctx := context.Background()
+	testExecMySQL(ctx, db, t)
+	testNamedExecMySQL(ctx, db, t)
+	testGetMySQL(ctx, db, t)
+	testSelectMySQL(ctx, db, t)
+	testQueryMySQL(ctx, db, t)
+	testRunInTxSuccessMySQL(ctx, db, t)
+	testRunInTxErrorMySQL(ctx, db, t)
+	testRunInTxRuntimePanicMySQL(ctx, db, t)
+	testRunInTxManualPanicMySQL(ctx, db, t)
+	testRunInTxNestMySQL(ctx, db, t)
 }
 
-func testDB(t *testing.T, db *DB) {
+func testExecMySQL(ctx context.Context, db *DB, t *testing.T) {
 	t.Helper()
 
-	var (
-		ctx context.Context = context.Background()
-	)
-
-	// --------------------
-	// Exec
-	// --------------------
-	{
-		u := newUser("exec@example.com", testPassword)
-		q := "INSERT INTO user (email, password) VALUES (?, ?);"
-		_, err := db.Exec(ctx, q, u.Email, u.Password)
-		if err != nil {
-			t.Fatal(err)
-		}
-		_, err = db.Exec(ctx, q, u.Email, u.Password) // duplicate entry
-		if err == nil {
-			t.Fatalf("want non-nil error")
-		}
+	u := newUser("exec@example.com", testPassword)
+	q := "INSERT INTO user (email, password) VALUES (?, ?);"
+	_, err := db.Exec(ctx, q, u.Email, u.Password)
+	if err != nil {
+		t.Fatal(err)
 	}
-
-	// --------------------
-	// NamedExec
-	// --------------------
-	{
-		u := newUser("namedExec@example.com", testPassword)
-		q := `INSERT INTO user (email, password) VALUES (:email, :password);`
-		_, err := db.NamedExec(ctx, q, u)
-		if err != nil {
-			t.Fatal(err)
-		}
-		_, err = db.NamedExec(ctx, q, u) // duplicate entry
-		if err == nil {
-			t.Fatalf("want non-nil error")
-		}
+	_, err = db.Exec(ctx, q, u.Email, u.Password) // duplicate entry
+	if err == nil {
+		t.Fatalf("want non-nil error")
 	}
+}
 
-	// --------------------
-	// Get
-	// --------------------
-	{
-		u := newUser("exec@example.com", testPassword)
-		q := "SELECT id, email, password FROM user WHERE email = ?;"
-		var got User
-		err := db.Get(ctx, &got, q, u.Email)
-		if err != nil {
-			t.Fatal(err)
-		}
-		if got.Email != u.Email {
-			t.Fatalf("wrong email: got %v, want %v", got.Email, u.Email)
-		}
-		if got.Password != u.Password {
-			t.Fatalf("wrong password: got %v, want %v", got.Password, u.Password)
-		}
-		err = db.Get(ctx, &got, q, "123456789@example.com")
-		if err == nil {
-			t.Fatalf("want non-nil error")
-		}
+func testNamedExecMySQL(ctx context.Context, db *DB, t *testing.T) {
+	t.Helper()
+
+	u := newUser("namedExec@example.com", testPassword)
+	q := `INSERT INTO user (email, password) VALUES (:email, :password);`
+	_, err := db.NamedExec(ctx, q, u)
+	if err != nil {
+		t.Fatal(err)
 	}
+	_, err = db.NamedExec(ctx, q, u) // duplicate entry
+	if err == nil {
+		t.Fatalf("want non-nil error")
+	}
+}
 
-	// --------------------
-	// Select
-	// --------------------
-	{
-		q := `SELECT id, email, password FROM user;`
-		var got []User
-		err := db.Select(ctx, &got, q)
+func testGetMySQL(ctx context.Context, db *DB, t *testing.T) {
+	t.Helper()
+
+	u := newUser("exec@example.com", testPassword)
+	q := "SELECT id, email, password FROM user WHERE email = ?;"
+	var got User
+	err := db.Get(ctx, &got, q, u.Email)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if got.Email != u.Email {
+		t.Fatalf("wrong email: got %v, want %v", got.Email, u.Email)
+	}
+	if got.Password != u.Password {
+		t.Fatalf("wrong password: got %v, want %v", got.Password, u.Password)
+	}
+	err = db.Get(ctx, &got, q, "123456789@example.com")
+	if err == nil {
+		t.Fatalf("want non-nil error")
+	}
+}
+
+func testSelectMySQL(ctx context.Context, db *DB, t *testing.T) {
+	t.Helper()
+
+	q := `SELECT id, email, password FROM user;`
+	var got []User
+	err := db.Select(ctx, &got, q)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if got, want := len(got), 2; got != want {
+		t.Fatalf("wrong len: got %v, want %v", got, want)
+	}
+	if got, want := got[0].Email, "exec@example.com"; got != want {
+		t.Fatalf("wrong email: got %v, want %v", got, want)
+	}
+	if got, want := got[0].Password, testPassword; got != want {
+		t.Fatalf("wrong password: got %v, want %v", got, want)
+	}
+	if got, want := got[1].Email, "namedExec@example.com"; got != want {
+		t.Fatalf("wrong email: got %v, want %v", got, want)
+	}
+	if got, want := got[1].Password, testPassword; got != want {
+		t.Fatalf("wrong password: got %v, want %v", got, want)
+	}
+}
+
+func testQueryMySQL(ctx context.Context, db *DB, t *testing.T) {
+	t.Helper()
+
+	q := `SELECT id, email, password FROM user;`
+	rows, err := db.Query(ctx, q)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if rows.Next() {
+		var u User
+		err := rows.StructScan(&u)
 		if err != nil {
 			t.Fatal(err)
 		}
-		if got, want := len(got), 2; got != want {
-			t.Fatalf("wrong len: got %v, want %v", got, want)
-		}
-		if got, want := got[0].Email, "exec@example.com"; got != want {
+		if got, want := u.Email, "exec@example.com"; got != want {
 			t.Fatalf("wrong email: got %v, want %v", got, want)
 		}
-		if got, want := got[0].Password, testPassword; got != want {
+		if got, want := u.Password, testPassword; got != want {
 			t.Fatalf("wrong password: got %v, want %v", got, want)
 		}
-		if got, want := got[1].Email, "namedExec@example.com"; got != want {
+	}
+	if rows.Next() {
+		var u User
+		err := rows.StructScan(&u)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if got, want := u.Email, "namedExec@example.com"; got != want {
 			t.Fatalf("wrong email: got %v, want %v", got, want)
 		}
-		if got, want := got[1].Password, testPassword; got != want {
+		if got, want := u.Password, testPassword; got != want {
 			t.Fatalf("wrong password: got %v, want %v", got, want)
 		}
 	}
-
-	// --------------------
-	// Query
-	// --------------------
-	{
-		q := `SELECT id, email, password FROM user;`
-		rows, err := db.Query(ctx, q)
-		if err != nil {
-			t.Fatal(err)
-		}
-		if rows.Next() {
-			var u User
-			err := rows.StructScan(&u)
-			if err != nil {
-				t.Fatal(err)
-			}
-			if got, want := u.Email, "exec@example.com"; got != want {
-				t.Fatalf("wrong email: got %v, want %v", got, want)
-			}
-			if got, want := u.Password, testPassword; got != want {
-				t.Fatalf("wrong password: got %v, want %v", got, want)
-			}
-		}
-		if rows.Next() {
-			var u User
-			err := rows.StructScan(&u)
-			if err != nil {
-				t.Fatal(err)
-			}
-			if got, want := u.Email, "namedExec@example.com"; got != want {
-				t.Fatalf("wrong email: got %v, want %v", got, want)
-			}
-			if got, want := u.Password, testPassword; got != want {
-				t.Fatalf("wrong password: got %v, want %v", got, want)
-			}
-		}
-		if rows.Next() {
-			t.Fatalf("want false")
-		}
+	if rows.Next() {
+		t.Fatalf("want false")
 	}
+}
 
-	// ------------------------------
-	// RunInTx (success)
-	// ------------------------------
-	{
-		s := newSession("tx-success", newUser("tx-success@example.com", testPassword))
-		txFn := func(ctx context.Context) error {
-			var err error
-			s, err = createUser(ctx, db, s)
-			if err != nil {
-				return err
-			}
-			s, err = createSession(ctx, db, s)
+func testRunInTxSuccessMySQL(ctx context.Context, db *DB, t *testing.T) {
+	t.Helper()
+
+	s := newSession("tx-success", newUser("tx-success@example.com", testPassword))
+	txFn := func(ctx context.Context) error {
+		var err error
+		s, err = createUser(ctx, db, s)
+		if err != nil {
 			return err
 		}
-
-		err, rbErr := db.RunInTx(ctx, txFn)
-		if rbErr != nil {
-			t.Fatal(err)
-		}
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		got, err := getSessionByID(ctx, db, s.ID)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		if diff := cmp.Diff(got, s); diff != "" {
-			t.Fatalf("wrong result: \n%s", diff)
-		}
+		s, err = createSession(ctx, db, s)
+		return err
 	}
 
-	// ------------------------------
-	// RunInTx (error)
-	// ------------------------------
-	{
-		s := newSession("tx-success", newUser("tx-error@example.com", testPassword))
-		txFn := func(ctx context.Context) error {
-			var err error
-			s, err = createUser(ctx, db, s) // success
-			if err != nil {
-				return err
-			}
-			s, err = createSession(ctx, db, s) // duplicate entry key 'tx-success'
+	err, rbErr := db.RunInTx(ctx, txFn)
+	if rbErr != nil {
+		t.Fatal(err)
+	}
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	got, err := getSessionByID(ctx, db, s.ID)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if diff := cmp.Diff(got, s); diff != "" {
+		t.Fatalf("wrong result: \n%s", diff)
+	}
+}
+
+func testRunInTxErrorMySQL(ctx context.Context, db *DB, t *testing.T) {
+	t.Helper()
+
+	s := newSession("tx-success", newUser("tx-error@example.com", testPassword))
+	txFn := func(ctx context.Context) error {
+		var err error
+		s, err = createUser(ctx, db, s) // success
+		if err != nil {
 			return err
 		}
-
-		err, rbErr := db.RunInTx(ctx, txFn)
-		if rbErr != nil {
-			t.Fatal(err)
-		}
-		if err == nil {
-			t.Fatal("want non-nil error")
-		}
-
-		_, err = getUserByEmail(ctx, db, s.User.Email)
-		if err == nil {
-			t.Fatal("want non-nil error")
-		} else if err != sql.ErrNoRows {
-			t.Fatal("want sql.ErrNoRows")
-		}
+		s, err = createSession(ctx, db, s) // duplicate entry key 'tx-success'
+		return err
 	}
 
-	// ------------------------------
-	// RunInTx (runtime panic)
-	// ------------------------------
-	{
-		s := newSession("tx-runtime-panic", newUser("tx-runtime-panic@example.com", testPassword))
+	err, rbErr := db.RunInTx(ctx, txFn)
+	if rbErr != nil {
+		t.Fatal(err)
+	}
+	if err == nil {
+		t.Fatal("want non-nil error")
+	}
+
+	_, err = getUserByEmail(ctx, db, s.User.Email)
+	if err == nil {
+		t.Fatal("want non-nil error")
+	} else if err != sql.ErrNoRows {
+		t.Fatal("want sql.ErrNoRows")
+	}
+}
+
+func testRunInTxRuntimePanicMySQL(ctx context.Context, db *DB, t *testing.T) {
+	t.Helper()
+
+	s := newSession("tx-runtime-panic", newUser("tx-runtime-panic@example.com", testPassword))
+	txFn := func(ctx context.Context) error {
+		var err error
+		s, err = createUser(ctx, db, s) // success
+		if err != nil {
+			return err
+		}
+		s, err = createSession(ctx, db, s) // success
+		if err != nil {
+			return err
+		}
+		fmt.Println([]string{}[99]) // runtime panic and rollback
+		return nil
+	}
+
+	err, rbErr := db.RunInTx(ctx, txFn)
+	if rbErr != nil {
+		t.Fatal(err)
+	}
+	if err == nil {
+		t.Fatal("want non-nil error")
+	} else if _, ok := err.(runtime.Error); !ok {
+		t.Fatal(err)
+	}
+
+	_, err = getUserByEmail(ctx, db, s.User.Email)
+	if err == nil {
+		t.Fatal("want non-nil error")
+	} else if err != sql.ErrNoRows {
+		t.Fatal("want sql.ErrNoRows")
+	}
+
+	_, err = getSessionByID(ctx, db, s.ID)
+	if err == nil {
+		t.Fatal("want non-nil error")
+	} else if err != sql.ErrNoRows {
+		t.Fatal("want sql.ErrNoRows")
+	}
+}
+
+func testRunInTxManualPanicMySQL(ctx context.Context, db *DB, t *testing.T) {
+	t.Helper()
+
+	s := newSession("tx-manual-panic", newUser("tx-manual-panic@example.com", testPassword))
+	txFn := func(ctx context.Context) error {
+		var err error
+		s, err = createUser(ctx, db, s) // success
+		if err != nil {
+			return err
+		}
+		s, err = createSession(ctx, db, s) // success
+		if err != nil {
+			return err
+		}
+		defer panic("manual panic!!!") // manual panic and rollback
+		return nil
+	}
+
+	err, rbErr := db.RunInTx(ctx, txFn)
+	if rbErr != nil {
+		t.Fatal(err)
+	}
+	if err == nil {
+		t.Fatal("want non-nil error")
+	}
+
+	_, err = getUserByEmail(ctx, db, s.User.Email)
+	if err == nil {
+		t.Fatal("want non-nil error")
+	} else if err != sql.ErrNoRows {
+		t.Fatal("want sql.ErrNoRows")
+	}
+
+	_, err = getSessionByID(ctx, db, s.ID)
+	if err == nil {
+		t.Fatal("want non-nil error")
+	} else if err != sql.ErrNoRows {
+		t.Fatal("want sql.ErrNoRows")
+	}
+}
+
+func testRunInTxNestMySQL(ctx context.Context, db *DB, t *testing.T) {
+	t.Helper()
+
+	s := newSession("tx-nest", newUser("tx-nest@example.com", testPassword))
+	txFn := func(ctx context.Context) error {
 		txFn := func(ctx context.Context) error {
 			var err error
 			s, err = createUser(ctx, db, s) // success
@@ -375,122 +455,34 @@ func testDB(t *testing.T, db *DB) {
 				return err
 			}
 			s, err = createSession(ctx, db, s) // success
-			if err != nil {
-				return err
-			}
-			fmt.Println([]string{}[99]) // runtime panic and rollback
-			return nil
-		}
-
-		err, rbErr := db.RunInTx(ctx, txFn)
-		if rbErr != nil {
-			t.Fatal(err)
-		}
-		if err == nil {
-			t.Fatal("want non-nil error")
-		} else if _, ok := err.(runtime.Error); !ok {
-			t.Fatal(err)
-		}
-
-		_, err = getUserByEmail(ctx, db, s.User.Email)
-		if err == nil {
-			t.Fatal("want non-nil error")
-		} else if err != sql.ErrNoRows {
-			t.Fatal("want sql.ErrNoRows")
-		}
-
-		_, err = getSessionByID(ctx, db, s.ID)
-		if err == nil {
-			t.Fatal("want non-nil error")
-		} else if err != sql.ErrNoRows {
-			t.Fatal("want sql.ErrNoRows")
-		}
-	}
-
-	// ------------------------------
-	// RunInTx (manual panic)
-	// ------------------------------
-	{
-		s := newSession("tx-manual-panic", newUser("tx-manual-panic@example.com", testPassword))
-		txFn := func(ctx context.Context) error {
-			var err error
-			s, err = createUser(ctx, db, s) // success
-			if err != nil {
-				return err
-			}
-			s, err = createSession(ctx, db, s) // success
-			if err != nil {
-				return err
-			}
-			defer panic("manual panic!!!") // manual panic and rollback
-			return nil
-		}
-
-		err, rbErr := db.RunInTx(ctx, txFn)
-		if rbErr != nil {
-			t.Fatal(err)
-		}
-		if err == nil {
-			t.Fatal("want non-nil error")
-		}
-
-		_, err = getUserByEmail(ctx, db, s.User.Email)
-		if err == nil {
-			t.Fatal("want non-nil error")
-		} else if err != sql.ErrNoRows {
-			t.Fatal("want sql.ErrNoRows")
-		}
-
-		_, err = getSessionByID(ctx, db, s.ID)
-		if err == nil {
-			t.Fatal("want non-nil error")
-		} else if err != sql.ErrNoRows {
-			t.Fatal("want sql.ErrNoRows")
-		}
-	}
-
-	// ------------------------------
-	// RunInTx (nest)
-	// ------------------------------
-	{
-		s := newSession("tx-nest", newUser("tx-nest@example.com", testPassword))
-		txFn := func(ctx context.Context) error {
-			txFn := func(ctx context.Context) error {
-				var err error
-				s, err = createUser(ctx, db, s) // success
-				if err != nil {
-					return err
-				}
-				s, err = createSession(ctx, db, s) // success
-				return err
-			}
-			err, _ := db.RunInTx(ctx, txFn) // nest
 			return err
 		}
+		err, _ := db.RunInTx(ctx, txFn) // nest
+		return err
+	}
 
-		err, rbErr := db.RunInTx(ctx, txFn)
-		if rbErr != nil {
-			t.Fatal(err)
-		}
-		if err == nil {
-			t.Fatal("want non-nil error")
-		} else if err != ErrNestedTransaction {
-			t.Fatal("want nested transaction error")
-		}
+	err, rbErr := db.RunInTx(ctx, txFn)
+	if rbErr != nil {
+		t.Fatal(err)
+	}
+	if err == nil {
+		t.Fatal("want non-nil error")
+	} else if err != ErrNestedTransaction {
+		t.Fatal("want nested transaction error")
+	}
 
-		_, err = getUserByEmail(ctx, db, s.User.Email)
-		if err == nil {
-			t.Fatal("want non-nil error")
-		} else if err != sql.ErrNoRows {
-			t.Fatal("want sql.ErrNoRows")
-		}
+	_, err = getUserByEmail(ctx, db, s.User.Email)
+	if err == nil {
+		t.Fatal("want non-nil error")
+	} else if err != sql.ErrNoRows {
+		t.Fatal("want sql.ErrNoRows")
+	}
 
-		_, err = getSessionByID(ctx, db, s.ID)
-		if err == nil {
-			t.Fatal("want non-nil error")
-		} else if err != sql.ErrNoRows {
-			t.Fatal("want sql.ErrNoRows")
-		}
+	_, err = getSessionByID(ctx, db, s.ID)
+	if err == nil {
+		t.Fatal("want non-nil error")
+	} else if err != sql.ErrNoRows {
+		t.Fatal("want sql.ErrNoRows")
 	}
 }
 
